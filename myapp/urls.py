76b47fd0bc1927a8a2eeeda4305ee8602a5acae6
naostmama <wkx1242888621@163.com>
2025-12-08 00:55:50 -0800

    FEAT: Apply Bootstrap styling to password change forms

--- myapp/urls.py
+++ myapp/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
 
 urlpatterns += [
     path('password_change/', auth_views.PasswordChangeView.as_view(
-        template_name='registration/password_change_form.html',
+        template_name='password_change_form.html',
         success_url='/profile/'  # redirect here after password change
     ), name='password_change'),
 
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'
+        template_name='password_change_done.html'
     ), name='password_change_done'),
 ]
